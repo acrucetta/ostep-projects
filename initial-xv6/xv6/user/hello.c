de7291489a14c9707d76fe110e3c2211684553ab
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-04-14 10:03:04 -0500

    updating xv6 to riscv

---
+++ initial-xv6/xv6/user/hello.c
@@ -0,0 +1,9 @@
+#include "kernel/fs.h"
+#include "kernel/stat.h"
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+  printf(1, "hello world\n");
+  exit(0);
+}
